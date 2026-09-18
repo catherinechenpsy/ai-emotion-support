1bf7c9eac7f77ce3bd838b9413f02080e7091b22
catherinechenpsy <catherinechen.psy@gmail.com>
2026-09-19 00:15:45 +0800

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,3 +1,3 @@
 import { neon } from '@neondatabase/serverless';
-const connectionString = postgresql://neondb_owner:npg_SHv8QXODm1Jg@ep-blue-butterfly-auqn5wv2-pooler.c-10.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require
+const connectionString = process.env.DATABASE_URL!;
 export const sql = neon(connectionString);
